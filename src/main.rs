061929c96d95997448c5f13f638fd4a200654b07
Santo Shakil <shakil.mezbah@gmail.com>
2023-06-01 21:34:42 +0600

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use web3::contract::{Contract, Options};
+use web3::types::{Address, TransactionReceipt};
+
 fn main() {
-    println!("Hello, world!");
+    // Create a new Web3 client.
+    let web3 = web3::Web3::new("http://localhost:8545");
+
+    // Get the address of the chat contract.
+    let chat_address = web3.eth().accounts().get(0).unwrap();
+
+    // Create a new instance of the chat contract.
+    let chat = Contract::new(chat_address, web3);
+
+    // Get the current list of users.
+    let users = chat.users().call().unwrap();
+
+    // Send a message to the user with the address "0x1234567890abcdef".
+    let message = "Hello, world!";
+    let receipt = chat
+        .send_message(message, "0x1234567890abcdef")
+        .send()
+        .unwrap();
+
+    // Check the status of the transaction receipt.
+    if receipt.status() == true {
+        println!("Message sent successfully!");
+    } else {
+        println!("Error sending message: {}", receipt.error());
+    }
 }
